fe1fba60311911d581b60eac262e1106ccaa03ae
= <=>
2019-10-07 16:12:29 +0700

    Barcode function is completed

---
+++ ManagerCinema/Custom_Form/BarcodeGenerator/BarcodeGenerator/TestApp.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace BarcodeLibTest
+{
+    partial class TestApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TestApp));
+            this.barcode = new System.Windows.Forms.PictureBox();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.btn_Save_Info = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnEncode = new System.Windows.Forms.Button();
+            this.lblEncodingTime = new System.Windows.Forms.Label();
+            this.chkGenerateLabel = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.barcode)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // barcode
+            // 
+            this.barcode.BackColor = System.Drawing.Color.Transparent;
+            this.barcode.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.barcode.Location = new System.Drawing.Point(3, 16);
+            this.barcode.Name = "barcode";
+            this.barcode.Size = new System.Drawing.Size(395, 341);
+            this.barcode.TabIndex = 13;
+            this.barcode.TabStop = false;
+            // 
+            // txtData
+            // 
+            this.txtData.Location = new System.Drawing.Point(4, 36);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(200, 20);
+            this.txtData.TabIndex = 1;
+            this.txtData.Text = "123456789";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 29;
+            this.label1.Text = "Giá trị";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_Exit);
+            this.groupBox1.Controls.Add(this.btn_Save_Info);
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Controls.Add(this.btnEncode);
+            this.groupBox1.Controls.Add(this.lblEncodingTime);
+            this.groupBox1.Controls.Add(this.chkGenerateLabel);
+            this.groupBox1.Controls.Add(this.txtData);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(218, 360);
+            this.groupBox1.TabIndex = 35;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin Phim";
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.Location = new System.Drawing.Point(12, 264);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(192, 43);
+            this.btn_Exit.TabIndex = 57;
+            this.btn_Exit.Text = "Đóng";
+            this.btn_Exit.UseVisualStyleBackColor = true;
+            this.btn_Exit.Click += new System.EventHandler(this.btn_Exit_Click);
+            // 
+            // btn_Save_Info
+            // 
+            this.btn_Save_Info.Location = new System.Drawing.Point(12, 134);
+            this.btn_Save_Info.Name = "btn_Save_Info";
+            this.btn_Save_Info.Size = new System.Drawing.Size(192, 43);
+            this.btn_Save_Info.TabIndex = 56;
+            this.btn_Save_Info.Text = "Lưu mã số";
+            this.btn_Save_Info.UseVisualStyleBackColor = true;
+            this.btn_Save_Info.Click += new System.EventHandler(this.btn_Save_Info_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(12, 198);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(192, 43);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Lưu ảnh";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnEncode
+            // 
+            this.btnEncode.Location = new System.Drawing.Point(12, 69);
+            this.btnEncode.Name = "btnEncode";
+            this.btnEncode.Size = new System.Drawing.Size(192, 43);
+            this.btnEncode.TabIndex = 3;
+            this.btnEncode.Text = "Tạo mã";
+            this.btnEncode.UseVisualStyleBackColor = true;
+            this.btnEncode.Click += new System.EventHandler(this.btnEncode_Click);
+            // 
+            // lblEncodingTime
+            // 
+            this.lblEncodingTime.AutoSize = true;
+            this.lblEncodingTime.Location = new System.Drawing.Point(86, 164);
+            this.lblEncodingTime.Name = "lblEncodingTime";
+            this.lblEncodingTime.Size = new System.Drawing.Size(0, 13);
+            this.lblEncodingTime.TabIndex = 45;
+            // 
+            // chkGenerateLabel
+            // 
+            this.chkGenerateLabel.AutoSize = true;
+            this.chkGenerateLabel.Location = new System.Drawing.Point(108, 19);
+            this.chkGenerateLabel.Name = "chkGenerateLabel";
+            this.chkGenerateLabel.Size = new System.Drawing.Size(72, 17);
+            this.chkGenerateLabel.TabIndex = 40;
+            this.chkGenerateLabel.Text = "Tạo nhãn";
+            this.chkGenerateLabel.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.barcode);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(401, 360);
+            this.groupBox2.TabIndex = 36;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Mã vạch";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BackColor = System.Drawing.SystemColors.Control;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox2);
+            this.splitContainer1.Size = new System.Drawing.Size(623, 360);
+            this.splitContainer1.SplitterDistance = 218;
+            this.splitContainer1.TabIndex = 37;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // TestApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 360);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(639, 399);
+            this.Name = "TestApp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tạo Vé";
+            this.Load += new System.EventHandler(this.TestApp_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.barcode)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox barcode;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnEncode;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.CheckBox chkGenerateLabel;
+        private System.Windows.Forms.Label lblEncodingTime;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+		private System.Windows.Forms.Button btn_Exit;
+		private System.Windows.Forms.Button btn_Save_Info;
+    }
+}
+
---
+++ ManagerCinema/Custom_Form/BarcodeGenerator/BarcodeGenerator/TestApp.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace BarcodeLibTest
+{
+    public partial class TestApp : Form
+    {
+        BarcodeLib.Barcode b = new BarcodeLib.Barcode();
+
+        public TestApp()
+        {
+            InitializeComponent();
+			btnSave.Enabled = false;
+			btn_Save_Info.Enabled = false;
+		}
+
+        private void TestApp_Load(object sender, EventArgs e)
+        {
+        }
+
+
+        private void btnEncode_Click(object sender, EventArgs e)
+        {
+            errorProvider1.Clear();
+			btnSave.Enabled = true;
+			btn_Save_Info.Enabled = true;
+			try
+            {
+                barcode.Image = b.Encode(BarcodeLib.TYPE.CODE39, txtData.Text.Trim(), b.ForeColor, b.BackColor, 300, 150, BarcodeLib.AlignmentPositions.CENTER, RotateFlipType.RotateNoneFlipNone, chkGenerateLabel.Checked, BarcodeLib.LabelPositions.BOTTOMCENTER);
+                barcode.Width = barcode.Image.Width;
+                barcode.Height = barcode.Image.Height;
+                //reposition the barcode image to the middle
+                barcode.Location = new Point((groupBox2.Location.X + groupBox2.Width / 2) - barcode.Width / 2, (groupBox2.Location.Y + groupBox2.Height / 2) - barcode.Height / 2);
+			}
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "BMP (*.bmp)|*.bmp|GIF (*.gif)|*.gif|JPG (*.jpg)|*.jpg|PNG (*.png)|*.png|TIFF (*.tif)|*.tif";
+            sfd.FilterIndex = 3;
+            sfd.AddExtension = true;
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                //BarcodeLib.SaveTypes savetype = BarcodeLib.SaveTypes.UNSPECIFIED;
+                ImageFormat savetype = ImageFormat.Jpeg;
+                switch (sfd.FilterIndex)
+                {
+                    case 1: /* BMP */  savetype = ImageFormat.Bmp; break;
+                    case 2: /* GIF */  savetype = ImageFormat.Gif; break;
+                    case 3: /* JPG */  savetype = ImageFormat.Jpeg; break;
+                    case 4: /* PNG */  savetype = ImageFormat.Png; break;
+                    case 5: /* TIFF */ savetype = ImageFormat.Tiff; break;
+                    default: break;
+                }
+                barcode.Image.Save(sfd.FileName, savetype);
+            }
+        }
+
+		private void btn_Exit_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void btn_Save_Info_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("Thêm dữ liệu vào SQL từ Button này !");
+		}
+
+        private void TxtWidth_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
