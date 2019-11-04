a3240ae723ec23a7af6ea3ae176ad7310c32f1c6
Long <tqlong1609@gmail.com>
2019-11-04 20:17:56 +0700

    update theme

--- ManagerCinema/BSLayer/MovieBS.cs
+++ ManagerCinema/BSLayer/MovieBS.cs
@@ -11,6 +11,7 @@ namespace ManagerCinema.BSLayer
     public class MovieBS
     {
         private DBMain dBMain;
+        string sqlString;
 
         public MovieBS()
         {
@@ -21,6 +22,14 @@ namespace ManagerCinema.BSLayer
         {
             string sqlString = "exec getAllMovies";
             return dBMain.ExecuteQueryDataSet(sqlString, CommandType.Text);
+        }
+
+        public bool addMovies(int id, string name, int time, string director, string country, 
+            string producer, string type, string actor, string date, string content, string timeStart,
+            string pathImage)
+        {
+ //           exec dbo.insertMovie @idMovie = '13', @name = '13', @time = '13', @director = '13', @country = '13',
+ //@producer = '13', @type = '13', @actor = '13', @date = '13', @content = '13', @timeStart = '13', @price = '13', @image = '13'
         }
     }
 }
