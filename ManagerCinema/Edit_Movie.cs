5a0dc09a75b95930746c4a70ab98e14d53fab414
= <=>
2019-11-18 16:12:32 +0700

    Add + Edit + Search Movie

---
+++ ManagerCinema/Edit_Movie.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using ManagerCinema.BSLayer;
+using ManagerCinema.DBLayer;
+
+namespace ManagerCinema
+{
+
+    public partial class Edit_Movie : Form
+    {
+        private DBMain dBMain;
+        string idMovie;
+        public Edit_Movie(string id)
+        {
+            InitializeComponent();
+            idMovie = id;
+        }
+
+        private void Edit_Movie_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            string time = txtTime.Text;
+            string director = textBox4.Text;
+            string country = txtCountry.Text;
+            string producer = txtProducer.Text;
+            string type = txtType.Text;
+            string Actor = txtActor.Text;
+            string Date = txtDate.Text;
+            string Content = txtContent.Text;
+            string Price = txtPrice.Text;
+
+            string sqlString = "Update Movie set Name = '" + name + "', Time = " + time + ", Director = " + director + ", Country = " + country + ", Producer = " + producer + ", Type = " + type + ", Actor = " + Actor + ", Date = " + Date + ", Content = " + Content + ", Price = " + Price + " where idMovie = " + idMovie;
+            DBMain dBMain = new DBMain();
+            try
+            {
+                dBMain.MyExecuteNonQuery(sqlString, CommandType.Text);
+                MessageBox.Show("Update completed");
+                this.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Update Failed!");
+                this.Close();
+            }
+        }
+    }
+}
